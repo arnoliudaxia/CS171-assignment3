db9491eb61edd31cfc618db654938133751041dc
Liuyifei <liuyf7@shanghaitech.edu.cn>
2022-11-11 20:08:21 +0800

    light相关完成

--- src/light.cpp
+++ src/light.cpp
@@ -3,28 +3,48 @@
 constexpr uint32_t SAMPLE_NUM = 16;
 
 Light::Light(const Vec3f &pos, const Vec3f &color) :
-    position(pos), color(color) {
+        position(pos), color(color) {
 
 }
+
 Vec3f Light::getColor() const {
-  return color;
+    return color;
+}
+
+Vec3f Light::getPosition() const {
+    return position;
 }
 
 SquareAreaLight::SquareAreaLight(const Vec3f &pos, const Vec3f &color, const Vec2f &dimension,
                                  const Vec3f &normal,
                                  const Vec3f &tangent) :
-    Light(pos, color),
-    rectangle(pos, dimension, normal, tangent) {}
+        Light(pos, color),
+        rectangle(pos, dimension, normal, tangent) {}
 
 std::vector<LightSample> SquareAreaLight::samples() const {
-  std::vector<LightSample> samples;
-  // TODO: Your code here.
-  assert(1 != 1);
-  return samples;
+    std::vector<LightSample> samples;
+    const int SAMPLES = 8; //请确保这是个偶数
+    Vec2f step=rectangle.getSize()/SAMPLES;
+    for (int i = 0; i < SAMPLES; i++) {
+        for (int j = 0; j < SAMPLES; j++) {
+            Vec3f pos = position + (i-SAMPLES/2) * step(0) * rectangle.getTangent() +
+                    (j-SAMPLES/2) * step(1)* rectangle.getNormal();
+            LightSample ls;
+            ls.position = pos;
+            ls.color = color;
+            samples.push_back(ls);
+
+        }
+    }
+    return samples;
 }
+
 bool SquareAreaLight::intersect(Ray &ray, Interaction &interaction) const {
-  // TODO: Your code here.
-    assert(1 != 1);
+    if(rectangle.intersect(ray,interaction))
+    {
+        interaction.type=Interaction::LIGHT;
+        return true;
+    }
     return false;
 }
 
